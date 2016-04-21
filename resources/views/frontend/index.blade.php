dd94a68b7ad064698ebf01683b69fe78587a4862
Nathon Shultz <nashultz07@gmail.com>
2016-04-20 23:51:14 -0500

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5,9 +5,5 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="content">
-        <div class="title">Faulkner County Landlords Association<br><small>Coming Soon!</small></div>
-    </div>
-</div>
+
 @endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -8,6 +8,45 @@
     <link href="themes/default/assets/css/app.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<div class="brand"><img src="#" alt="Faulkner County Landlord Association"> Faulkner County Landlord Association</div>
+
+<!-- Navigation -->
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+            <a class="navbar-brand" href="index.html">Business Casual</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="blog.html">Blog</a>
+                </li>
+                <li>
+                    <a href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+
 <div class="container">
     @yield('content')
 </div>
