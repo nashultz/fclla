b1062c507cb5e68a9801512bf2b54d4d47bc1fc6
Nathon Shultz <nashultz07@gmail.com>
2016-04-21 21:32:06 -0500

    up

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <h3>Welcome to the Faulkner County Landlord Association</h3>
+            <h2>Welcome to the Faulkner County Landlord Association</h2>
             <div>
                 The Faulkner County Landlord Association (FCLLA) holds monthly meetings with fellow landlords for the purpose of networking & educating one another.
                 The FCLLA has guest speakers each month that discusses current issues or problems that affect our industry.
@@ -17,6 +17,39 @@
                 also secured discounts at participating businesses in the area for active FCLLA members. We also draw for door prizes
                 each month at our meetings.<br>
                 The Faulkner County Library located at 1900 Tyler St. in Conway, Arkansas. The meetings start at 7:00 pm.
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>2015-2016 Officers</h3>
+            <div class="col-lg-6 col-lg-offset-3">
+                <table class="table table-responsive table-striped">
+                    <tbody>
+                        <tr>
+                            <td><strong>President</strong></td>
+                            <td>Mark Burrier</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Vice President</strong></td>
+                            <td>Belina Boyd</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Treasurer</strong></td>
+                            <td>Connie Trent</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Secretary</strong></td>
+                            <td>Chris Gilden</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Board Director</strong></td>
+                            <td>Jerrill Smith</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
