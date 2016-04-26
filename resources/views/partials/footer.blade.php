febc77e5c952b1c8dcd7b2ac48a90bfb97e2d2ce
Nathon Shultz <nashultz07@gmail.com>
2016-04-25 23:10:14 -0500

    update

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,43 @@
+<!-- Footer -->
+<footer class="text-center">
+    <div class="footer-above">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col col-md-4">
+                    <h3>Location</h3>
+                    <p>Faulkner County Library<br>1900 Tyler Street<br>Conway, AR  72034</p>
+                </div>
+                <div class="footer-col col-md-4">
+                    <h3>Around the Web</h3>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="http://www.facebook.com/groups/fclla" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-col col-md-4">
+                    <h3>About FCLLA</h3>
+                    <p>Faulkner County Landlord Association exists to provide our members with opportunities to become
+                        more successful and profitable property managers by networking with other area landlords, offering
+                        educational programs tailored to our individual businesses, and building a united voice to local
+                        and state law makers.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center"><a href="{{route('disclaimers')}}" title="Disclaimers">Disclaimers</a> &mdash; <a href="{{route('mission')}}" title="Mission Statement">Mission Statement</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-below">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    Copyright &copy; Faulkner County Landlord Association {{date('Y')}}. Site maintained by <a href="http://nathonshultz.com" target="_blank">Nathon Shultz</a>.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
