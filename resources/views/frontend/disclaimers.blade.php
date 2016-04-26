9f48d1d6f67091cc20d2cdb470ef63df64acdc8f
Nathon Shultz <nashultz07@gmail.com>
2016-04-25 22:57:06 -0500

    updates

---
+++ resources/views/frontend/disclaimers.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.frontendalt')
+
+@section('page_title')
+    Join
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <p class="col-lg-8 col-lg-offset-2">
+            <h2>Faulkner County Landlord Association Disclaimers</h2>
+            <p>The Faulkner County Landlord Association ("FCLLA") is a non-profit organization and does not give legal,
+                tax, economic or investment advice. FCLLA does not endorse any person, product, literature, organization,
+                company or advertiser. The information and material received at the meetings or on our website are for
+                educational, motivational and enjoyable purposes.</p>
+            <p>FCLLA disclaims all liability for any action or inaction taken or not taken as a result of its communications
+                from or to the guest speakers, members, officers or directors.</p>
+        </div><div class="clearfix"></div>
+    </div>
+</div><div class="clearfix"></div>
+@endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -55,19 +55,7 @@
                     <h3>Around the Web</h3>
                     <ul class="list-inline">
                         <li>
-                            <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            <a href="http://www.facebook.com/groups/fclla" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
                         </li>
                     </ul>
                 </div>
@@ -77,6 +65,12 @@
                         more successful and profitable property managers by networking with other area landlords, offering
                         educational programs tailored to our individual businesses, and building a united voice to local
                         and state law makers.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="row">
+                <div class="footer-col col-lg-12">
+                    <div class="text-center"><a href="{{route('disclaimers')}}" title="Disclaimers">Disclaimers</a> &mdash; <a href="{{route('mission')}}" title="Mission Statement">Mission Statement</a></div>
                 </div>
             </div>
         </div>
