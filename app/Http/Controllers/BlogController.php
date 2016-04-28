4942e954398646e177df90d29b5d3d7cb8154f35
Nathon Shultz <nashultz07@gmail.com>
2016-04-27 20:49:35 -0500

    edit

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -18,7 +18,7 @@ class BlogController extends Controller
 
     public function index()
     {
-        $posts = $this->posts->with('author')->latest()->get();
+        $posts = $this->posts->latest()->get();
         return view('frontend.blog.index', compact('posts'));
     }
 }
