75ce255f79d4e304c853f0f3bb0d1378f0cd5903
Nathon Shultz <nashultz07@gmail.com>
2016-04-27 20:43:22 -0500

    update

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $table = 'posts';
 
+    protected $fillable = ['author_id','title','slug','excerpt','body','published_at'];
+
+    public function scopeLatest($query)
+    {
+        $query->orderBy('published_at','desc');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
