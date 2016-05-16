715f5ba2eddfb9aa5508b1b4f031ec2fd8c78b8c
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-05-16 17:57:37 -0500

    Login form test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,16 +11,28 @@
 |
 */
 
+/* HOME ROUTE */
 Route::get('/', 'PagesController@index')->name('home');
 
+/* JOIN ROUTE */
 Route::get('join', 'PagesController@join')->name('join');
 
+/* CONTACT ROUTES */
 Route::get('contact', 'ContactController@index')->name('contact');
 Route::post('contact', 'ContactController@send')->name('sendContact');
 
+/* LEGAL ROUTE */
 Route::get('legal', 'PagesController@legal')->name('legal');
 
+/* APPLICATION ROUTES */
 Route::get('download/application', 'PagesController@downloadapp')->name('downloadapp');
 
+/* BLOG ROUTES */
 Route::get('news', 'BlogController@index')->name('blogindex');
 Route::get('news/{post}', 'BlogController@view')->name('blogpost');
+
+/* MEMBER ROUTES */
+Route::get('member/login', 'Auth\AuthController@getLogin')->name('member.login');
+Route::post('member/login', 'Auth\AuthController@postLogin');
+Route::get('member/logout', 'Auth\AuthController@logout')->name('member.logout');
+
--- public/themes/default/views/partials/footer.blade.php
+++ public/themes/default/views/partials/footer.blade.php
@@ -26,7 +26,30 @@
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="text-center"><a href="{{route('legal')}}" title="Mission Statement and Legal Information">Legal</a></div>
+                    <div class="text-center">
+                        <a href="{{route('legal')}}" title="Mission Statement and Legal Information">Legal</a>
+                        <a data-toggle="modal" href="#myLogin" >Member Login</a>
+
+                        <div class="modal hide" id="myLogin">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">x</button>
+                                <h3>Login</h3>
+                            </div>
+                            <div class="modal-body">
+                                <form method="post" action='' name="login_form">
+                                    <p><input type="text" class="span3" name="eid" id="email" placeholder="Email"></p>
+                                    <p><input type="password" class="span3" name="passwd" placeholder="Password"></p>
+                                    <p><button type="submit" class="btn btn-primary">Sign in</button>
+                                        <a href="#">Forgot Password?</a>
+                                    </p>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                New To MyWebsite.com?
+                                <a href="#" class="btn btn-primary">Register</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
