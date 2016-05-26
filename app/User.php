1cd3dfc116df7dff43c60ed691eff613a0fdbda5
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-05-26 00:08:44 -0500

    test2

--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function getIsAdminAttribute()
+    {
+        return $this->admin;
+    }
 }
