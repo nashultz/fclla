3fb0177b2b650e2ca5dacdc97350285f169b5f0e
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-05-26 00:16:49 -0500

    update

--- public/themes/default/views/partials/submembernav.blade.php
+++ public/themes/default/views/partials/submembernav.blade.php
@@ -4,7 +4,7 @@
         <div class="col-lg-6">
             <ul class="list-unstyled">
                 @if(Auth::check())
-                    @if(Auth::user()->is_admin)
+                    @if(Auth::user()->admin)
                         <li>
                             <a href="{{route('member::newpost')}}">New Post</a>
                         </li>
