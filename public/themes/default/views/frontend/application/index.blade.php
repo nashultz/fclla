98fd16349819bb8ab6b750389619f243bf78ec2c
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-05-30 12:02:19 -0500

    update

--- public/themes/default/views/frontend/application/index.blade.php
+++ public/themes/default/views/frontend/application/index.blade.php
@@ -13,9 +13,26 @@
     </div>
     <div class="row">
         <div class="col-lg-6 col-lg-offset-3">
-            <p>
+            {!!Form::open([route('saveapp')])!!}
+            <div class="form-group{{ $errors->has('bname') ? ' has-error' : '' }}">
+                {!!Form::label('bname', 'Business Name:', ['class'=>'col-md-4 control-label'])!!}
 
-            </p>
+                <div class="col-md-6">
+                    {!!Form::text('bname',null,['class'=>'form-control'])!!}
+
+                    @if ($errors->has('bname'))
+                        <span class="help-block">
+                                    <strong>{{ $errors->first('bname') }}</strong>
+                                </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    {!!Form::submit('Apply',['class'=>'btn btn-warning'])!!}
+                </div>
+            </div>
+            {!!Form::close()!!}
         </div><div class="clearfix"></div>
     </div>
 </div><div class="clearfix"></div>
