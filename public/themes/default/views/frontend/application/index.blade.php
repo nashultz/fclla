cd385ca0ee24909c117748951da83477951891d7
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-05-30 12:11:37 -0500

    add

--- public/themes/default/views/frontend/application/index.blade.php
+++ public/themes/default/views/frontend/application/index.blade.php
@@ -27,6 +27,59 @@
                     @endif
                 </div>
             </div>
+            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                {!!Form::label('name', 'Name:', ['class'=>'col-md-4 control-label'])!!}
+
+                <div class="col-md-6">
+                    {!!Form::text('name',null,['class'=>'form-control'])!!}
+
+                    @if ($errors->has('name'))
+                        <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('address|city|state|zipcode') ? ' has-error' : '' }}">
+                {!!Form::label('address', 'Address:', ['class'=>'col-md-12 control-label'])!!}
+
+                <div class="col-md-5">
+                    {!!Form::text('address',null,['class'=>'form-control'])!!}
+
+                    @if ($errors->has('address'))
+                        <span class="help-block">
+                                <strong>{{ $errors->first('address') }}</strong>
+                            </span>
+                    @endif
+                </div>
+                <div class="col-md-3">
+                    {!!Form::text('city',null,['class'=>'form-control'])!!}
+
+                    @if ($errors->has('city'))
+                        <span class="help-block">
+                                <strong>{{ $errors->first('city') }}</strong>
+                            </span>
+                    @endif
+                </div>
+                <div class="col-md-2">
+                    {!!Form::text('state',null,['class'=>'form-control'])!!}
+
+                    @if ($errors->has('state'))
+                        <span class="help-block">
+                                <strong>{{ $errors->first('state') }}</strong>
+                            </span>
+                    @endif
+                </div>
+                <div class="col-md-2">
+                    {!!Form::text('zipcode',null,['class'=>'form-control'])!!}
+
+                    @if ($errors->has('zipcode'))
+                        <span class="help-block">
+                                <strong>{{ $errors->first('zipcode') }}</strong>
+                            </span>
+                    @endif
+                </div>
+            </div>
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
                     {!!Form::submit('Apply',['class'=>'btn btn-warning'])!!}
