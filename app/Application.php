82a55cf89fd70315f859fa7200012ec8d5b8ac52
Nathon Shultz <nashultz07@gmail.com>
2016-06-06 12:29:14 -0500

    update

--- app/Application.php
+++ app/Application.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    protected $fillable = [
+        'bname', 'name', 'email', 'address', 'city', 'state', 'zipcode', 'bphone', 'hphone', 'cphone',
+        'units', 'membership', 'roster'
+    ];
 }
