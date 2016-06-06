15e6743feaaa0d37258a8d413b7046158201b6c0
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-06-06 16:38:41 -0500

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,13 +26,13 @@ Route::get('news/{slug}', 'BlogController@view')->name('blogpost');
 /* MEMBER ROUTES*/
 Route::auth();
 
-group(['as'=>'member::', 'prefix'=>'member'], function() {
-    get('newpost', 'BlogController@newpost')->name('newpost');
-    post('addpost', 'BlogController@addpost')->name('addpost');
+Route::group(['as'=>'member::', 'prefix'=>'member'], function() {
+    Route::get('newpost', 'BlogController@newpost')->name('newpost');
+    Route::post('addpost', 'BlogController@addpost')->name('addpost');
 })->middleware('auth');
 
 /* ADMIN ROUTES*/
 
-group(['as'=>'admin::', 'prefix'=>'admin'], function() {
-    get('applications', 'ApplicationController@viewAll')->name('viewallapps');
+Route::group(['as'=>'admin::', 'prefix'=>'admin'], function() {
+    Route::get('applications', 'ApplicationController@viewAll')->name('viewallapps');
 })->middleware('auth');
