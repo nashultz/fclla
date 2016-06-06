a7258ba927b34350c09498e51750a37dbe4fa689
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-06-06 16:37:43 -0500

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,46 +3,36 @@
 /* HOME ROUTE */
 Route::get('/', 'PagesController@index')->name('home');
 
-/* JOIN ROUTE
-Route::get('join', 'PagesController@join')->name('join');*/
+/* JOIN ROUTE */
+Route::get('join', 'PagesController@join')->name('join');
 
-/* CONTACT ROUTES
+/* CONTACT ROUTES */
 Route::get('contact', 'ContactController@index')->name('contact');
-Route::post('contact', 'ContactController@send')->name('sendContact');*/
+Route::post('contact', 'ContactController@send')->name('sendContact');
 
-/* LEGAL ROUTE
-Route::get('legal', 'PagesController@legal')->name('legal');*/
+/* LEGAL ROUTE*/
+Route::get('legal', 'PagesController@legal')->name('legal');
 
-/* APPLICATION ROUTES
+/* APPLICATION ROUTES*/
 Route::get('application/download', 'ApplicationController@downloadapp')->name('downloadapp');
 Route::get('application', 'ApplicationController@index')->name('application');
 Route::post('application', 'ApplicationController@save')->name('saveapp');
-Route::get('application/{application}/print', 'ApplicationController@printfilledapp')->name('filledapp');*/
+Route::get('application/{application}/print', 'ApplicationController@printfilledapp')->name('filledapp');
 
-/* BLOG ROUTES
+/* BLOG ROUTES*/
 Route::get('news', 'BlogController@index')->name('blogindex');
-Route::get('news/{slug}', 'BlogController@view')->name('blogpost');*/
+Route::get('news/{slug}', 'BlogController@view')->name('blogpost');
 
-/* MEMBER ROUTES
-Route::auth();*/
+/* MEMBER ROUTES*/
+Route::auth();
 
-/*group(['as'=>'member::', 'prefix'=>'member'], function() {
+group(['as'=>'member::', 'prefix'=>'member'], function() {
     get('newpost', 'BlogController@newpost')->name('newpost');
     post('addpost', 'BlogController@addpost')->name('addpost');
-})->middleware('auth');*/
+})->middleware('auth');
 
-
-
-
-
-
-
-
-
-
-
-/* ADMIN ROUTES
+/* ADMIN ROUTES*/
 
 group(['as'=>'admin::', 'prefix'=>'admin'], function() {
     get('applications', 'ApplicationController@viewAll')->name('viewallapps');
-})->middleware('auth');*/
+})->middleware('auth');
