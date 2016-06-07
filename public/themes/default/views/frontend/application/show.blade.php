8e3d9f49fcb24899b3d7776619cfc7674c59c6e2
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-06-07 02:08:08 -0500

    update

--- public/themes/default/views/frontend/application/show.blade.php
+++ public/themes/default/views/frontend/application/show.blade.php
@@ -100,6 +100,7 @@
                 </tr>
             </tbody>
         </table>
+        <a href="" class="btn btn-success"><i class="fa fa-check"></i> Accept</a><a href="" class="btn btn-danger"><i class="fa fa-close"></i> Deny</a>
     </div>
 </div>
 @endsection
