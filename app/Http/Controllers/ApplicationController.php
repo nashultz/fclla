48faaf4f323c236f9651db47ab3365d66380cde9
Nathon Shultz <nashultz07@gmail.com>
2016-06-09 20:54:51 -0500

    event testing

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -43,7 +43,7 @@ class ApplicationController extends Controller
         $this->application->save($data);
 
         flash()->success('Application was successfully sent!');
-        dd($this->application);
+        event($this->application);
         return redirect()->back();
     }
 
