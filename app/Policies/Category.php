255aa9d4b1e1221dadee0f2da961889b3f2ceaca
Nathon Shultz <nashultz07@gmail.com>
2016-06-30 10:52:48 -0500

    forum testing

--- app/Policies/Category.php
+++ app/Policies/Category.php
@@ -6,11 +6,168 @@ use Riari\Forum\Policies\CategoryPolicy;
 
 class Category extends CategoryPolicy {
 
-    public function __construct($user, Category $category)
+    /**
+     * Permission: Create threads in category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function createThreads($user, Category $category)
     {
         if($user->admin != 1) {
             return false;
         }
+
+        return true;
+    }
+
+    /**
+     * Permission: Manage threads in category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function manageThreads($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return $this->deleteThreads($user, $category) ||
+        $this->enableThreads($user, $category) ||
+        $this->moveThreadsFrom($user, $category) ||
+        $this->lockThreads($user, $category) ||
+        $this->pinThreads($user, $category);
+    }
+
+    /**
+     * Permission: Delete threads in category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function deleteThreads($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Enable threads in category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function enableThreads($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Move threads from category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function moveThreadsFrom($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Move threads to category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function moveThreadsTo($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Lock threads in category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function lockThreads($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Pin threads in category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function pinThreads($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: View category. Only takes effect for 'private' categories.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function view($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Delete category.
+     *
+     * @param  object  $user
+     * @param  Category  $category
+     * @return bool
+     */
+    public function delete($user, Category $category)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
     }
 
 }
--- app/Policies/Forum.php
+++ app/Policies/Forum.php
@@ -6,11 +6,102 @@ use Riari\Forum\Policies\ForumPolicy;
 
 class Forum extends ForumPolicy {
 
-    public function __construct($user)
+    /**
+     * Permission: Create categories.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function createCategories($user)
     {
         if($user->admin != 1) {
             return false;
         }
+
+        return true;
+    }
+
+    /**
+     * Permission: Manage category.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function manageCategories($user)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return $this->moveCategories($user) ||
+        $this->renameCategories($user);
+    }
+
+    /**
+     * Permission: Move categories.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function moveCategories($user)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Rename categories.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function renameCategories($user)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: Mark new/updated threads as read.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function markNewThreadsAsRead($user)
+    {
+        return true;
+    }
+
+    /**
+     * Permission: View trashed threads.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function viewTrashedThreads($user)
+    {
+        if($user->admin != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Permission: View trashed posts.
+     *
+     * @param  object  $user
+     * @return bool
+     */
+    public function viewTrashedPosts($user)
+    {
+        return true;
     }
 
 }
