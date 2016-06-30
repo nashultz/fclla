e94942f4fdfef5c63ec4b86834ee44a7f9f2da3e
Nathon Shultz <nashultz07@gmail.com>
2016-06-29 22:34:15 -0500

    approve testing

--- app/Http/Files/Auth/CreateNewPassword.php
+++ app/Http/Files/Auth/CreateNewPassword.php
@@ -26,7 +26,7 @@ class CreateNewPassword
 
     public function sendPassEmail($newPass)
     {
-        $u = array('newPass'=>$newPass, $this->user);
+        $u = array('newPass'=>$newPass, $this->user->toArray());
         dd($u);
         Mail::send('emails.tempuserpass', $this->user,  function($m) use ($u) {
             $m->from('info@fclla.org', 'Faulkner County Landlord Association');
