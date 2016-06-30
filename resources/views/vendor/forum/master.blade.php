c33a03d659cf0bf3188152bb10fffe42a7f32d4c
Nathon Shultz <nashultz07@gmail.com>
2016-06-30 10:21:56 -0500

    forum testing

--- resources/views/vendor/forum/master.blade.php
+++ resources/views/vendor/forum/master.blade.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.frontendalt')
 
-    <title>
+@section('page_title')
         @if (isset($thread))
             {{ $thread->title }} -
         @endif
@@ -12,30 +8,9 @@
             {{ $category->title }} -
         @endif
         {{ trans('forum::general.home_title') }}
-    </title>
+@endsection
 
-    <!-- jQuery -->
-    <script src="//code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-
-    <!-- Bootstrap -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-    <style>
-    body {
-        padding: 30px 0;
-    }
-
-    textarea {
-        min-height: 200px;
-    }
-
-    .deleted {
-        opacity: 0.65;
-    }
-    </style>
-</head>
-<body>
+@section('content')
     <div class="container">
         @include ('forum::partials.breadcrumbs')
         @include ('forum::partials.alerts')
@@ -109,5 +84,4 @@
     </script>
 
     @yield('footer')
-</body>
-</html>
+@endsection
