0aa9e629c647b27b7742a1b831f6256911cf88e2
Nathon Shultz <nashultz07@gmail.com>
2016-06-30 09:22:57 -0500

    testing

--- database/seeds/ApplicationsTableSeeder.php
+++ database/seeds/ApplicationsTableSeeder.php
@@ -11,18 +11,6 @@ class ApplicationsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('applications')->insert([
-            'bname' => 'FCLLA Team',
-            'name' => 'Nathon Shultz',
-            'address' => 'Some Address',
-            'city' => 'Conway',
-            'state' => 'AR',
-            'zipcode' => '72034',
-            'cphone' => '5019089587',
-            'email' => 'nathon@nathonshultz.com',
-            'units' => 2,
-            'membership' => 0,
-            'roster' => 0
-        ]);
+        //
     }
 }
