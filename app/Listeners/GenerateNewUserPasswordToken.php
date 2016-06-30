4096a21abe8b55f91725958882f3b7d84b84fb5a
Nathon Shultz <nashultz07@gmail.com>
2016-06-29 20:39:51 -0500

    approve testing

--- app/Listeners/GenerateNewUserPasswordToken.php
+++ app/Listeners/GenerateNewUserPasswordToken.php
@@ -29,7 +29,6 @@ class GenerateNewUserPasswordToken
     public function handle(NewUserAccountCreated $event)
     {
         $user = $event->user;
-        dd($user);
         $newUser = User::create($user);
         new CreateNewPassword($newUser);
     }
