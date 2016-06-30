6689997d2db5010d1fa17334c4ae385547f30bde
Nathon Shultz <nashultz07@gmail.com>
2016-06-30 10:47:06 -0500

    forum testing

--- app/Policies/Category.php
+++ app/Policies/Category.php
@@ -6,7 +6,7 @@ use Riari\Forum\Policies\CategoryPolicy;
 
 class Category extends CategoryPolicy {
 
-    public function __construct($user)
+    public function __construct($user, Category $category)
     {
         if($user->admin != 1) {
             return false;
