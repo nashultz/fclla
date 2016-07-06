0957521a70590d9c584be43c583d9f7d19af0713
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-07-06 01:33:38 -0500

    updating flare assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,7 @@
 
         <!-- JavaScript -->
         <script src="{{asset('assets/js/main.js')}}"></script>
+        <script src="{{asset('assets/js/app.js')}}"></script>
         <script src="{{asset('js/sweetalert.min.js')}}"></script>
     </div>
 </body>
