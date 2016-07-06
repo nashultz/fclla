6c968528567f7abcea29d4beeb650c621a16c91b
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-07-06 02:43:58 -0500

    updating base code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,8 @@
 <?php
 
 /* HOME ROUTE */
-//Route::get('/', 'PagesController@index')->name('home');
-Route::get('/', 'WelcomeController@show');
+Route::get('/', 'PagesController@index')->name('home');
+//Route::get('/', 'WelcomeController@show');
 
 /*Route::get('home', function() {
     return redirect('/');
@@ -11,38 +11,38 @@ Route::get('home', 'WelcomeController@home');
 
 
 /* JOIN ROUTE */
-//Route::get('join', 'PagesController@join')->name('join');
-//
-///* CONTACT ROUTES */
-//Route::get('contact', 'ContactController@index')->name('contact');
-//Route::post('contact', 'ContactController@send')->name('sendContact');
-//
-///* LEGAL ROUTE*/
-//Route::get('legal', 'PagesController@legal')->name('legal');
-//
-///* APPLICATION ROUTES*/
-//Route::get('application/download', 'ApplicationController@downloadapp')->name('downloadapp');
-//Route::get('application', 'ApplicationController@index')->name('application');
-//Route::post('application', 'ApplicationController@save')->name('saveapp');
-//
-///* BLOG ROUTES*/
-//Route::get('news', 'BlogController@index')->name('blogindex');
-//Route::get('news/{slug}', 'BlogController@view')->name('blogpost');
-//
-///* MEMBER ROUTES*/
+Route::get('join', 'PagesController@join')->name('join');
+
+/* CONTACT ROUTES */
+Route::get('contact', 'ContactController@index')->name('contact');
+Route::post('contact', 'ContactController@send')->name('sendContact');
+
+/* LEGAL ROUTE*/
+Route::get('legal', 'PagesController@legal')->name('legal');
+
+/* APPLICATION ROUTES*/
+Route::get('application/download', 'ApplicationController@downloadapp')->name('downloadapp');
+Route::get('application', 'ApplicationController@index')->name('application');
+Route::post('application', 'ApplicationController@save')->name('saveapp');
+
+/* BLOG ROUTES*/
+Route::get('news', 'BlogController@index')->name('blogindex');
+Route::get('news/{slug}', 'BlogController@view')->name('blogpost');
+
+/* MEMBER ROUTES*/
 //Route::auth();
-//
-//Route::group(['as'=>'member::', 'prefix'=>'member', 'middleware'=>'auth'], function() {
-//    Route::get('newpost', 'BlogController@newpost')->name('newpost');
-//    Route::post('addpost', 'BlogController@addpost')->name('addpost');
-//});
-//
-///* ADMIN ROUTES*/
-//
-//Route::group(['as'=>'admin::', 'prefix'=>'admin', 'middleware'=>'admin'], function() {
-//    Route::get('applications', 'ApplicationController@viewAll')->name('viewallapps');
-//    Route::get('application/{application}/print', 'ApplicationController@printfilledapp')->name('filledapp');
-//    Route::get('application/{application}/approve', 'ApplicationController@approve')->name('approveapp');
-//    Route::get('application/{application}/deny', 'ApplicationController@deny')->name('denyapp');
-//    Route::get('application/{application}', 'ApplicationController@view')->name('viewapp');
-//});
+
+Route::group(['as'=>'member::', 'prefix'=>'member', 'middleware'=>'auth'], function() {
+    Route::get('newpost', 'BlogController@newpost')->name('newpost');
+    Route::post('addpost', 'BlogController@addpost')->name('addpost');
+});
+
+/* ADMIN ROUTES*/
+
+Route::group(['as'=>'admin::', 'prefix'=>'admin', 'middleware'=>'admin'], function() {
+    Route::get('applications', 'ApplicationController@viewAll')->name('viewallapps');
+    Route::get('application/{application}/print', 'ApplicationController@printfilledapp')->name('filledapp');
+    Route::get('application/{application}/approve', 'ApplicationController@approve')->name('approveapp');
+    Route::get('application/{application}/deny', 'ApplicationController@deny')->name('denyapp');
+    Route::get('application/{application}', 'ApplicationController@view')->name('viewapp');
+});
