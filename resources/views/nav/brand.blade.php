8ad82670fa7f27a042b393987d48ceacb02c98c9
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-07-06 02:33:59 -0500

    updating flare assets

--- resources/views/nav/brand.blade.php
+++ resources/views/nav/brand.blade.php
@@ -1,3 +1,3 @@
 <a class="navbar-brand" href="/home">
-    <img src="/img/logo.jpg" style="height: 32px;"> Faulkner County Landlords Association
+    <img src="/img/logo.jpg" style="height: 32px;"> &nbsp; Faulkner County Landlords Association
 </a>
