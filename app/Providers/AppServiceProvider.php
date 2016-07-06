5ec9dd7cf249e1c6159e1851886cc71d953460e8
Nathon Shultz <nashultz07@gmail.com>
2016-07-05 23:03:29 -0500

    update regular routes to test flare routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,15 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('theme.finder', function($app){
-            $finder = new ThemeViewFinder($app['files'], $app['config']['view.paths']);
-
-            $config = $app['config']['cms.theme'];
-
-            $finder->setBasePath($app['path.public'].'/'.$config['folder']);
-            $finder->setActiveTheme($config['active']);
-
-            return $finder;
-        });
+        
     }
 }
