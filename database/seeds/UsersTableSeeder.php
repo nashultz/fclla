d6fe00117e2e2895a822eaa694afdd7b27942ce7
Nathon Scott <nashultz07@gmail.com>
2018-04-02 20:00:42 -0500

    update
    update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,26 +11,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Nathon Shultz',
-            'email' => 'nashultz07@gmail.com',
-            'password' => bcrypt('secret'),
-            'admin' => 1
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'Nathon Shultz',
+        //     'email' => 'nashultz07@gmail.com',
+        //     'password' => bcrypt('secret'),
+        //     'admin' => 1
+        // ]);
 
-        DB::table('users')->insert([
-            'name' => 'FCLLA Team',
-            'email' => 'info@fclla.org',
-            'password' => bcrypt('secret'),
-            'admin' => 1
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'FCLLA Team',
+        //     'email' => 'info@fclla.org',
+        //     'password' => bcrypt('secret'),
+        //     'admin' => 1
+        // ]);
 
-        DB::table('users')->insert([
-            'name' => 'Jessica Burrier',
-            'email' => 'jessica@fclla.org',
-            'password' => bcrypt('secret'),
-            'admin' => 1
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'Jessica Burrier',
+        //     'email' => 'jessica@fclla.org',
+        //     'password' => bcrypt('secret'),
+        //     'admin' => 1
+        // ]);
 
         DB::table('users')->insert([
             'name' => 'Kristine Gilden',
