717cd77aa4e2fe2230c5a252436b4e39f542cf2f
Nathon Scott <nashultz07@gmail.com>
2018-04-29 04:28:53 -0500

    updates

--- public/themes/default/views/frontend/join.blade.php
+++ public/themes/default/views/frontend/join.blade.php
@@ -41,19 +41,19 @@
                 </ul>
             </p>
             <p><a href="{{route('application')}}" title="Online Application" class="btn btn-primary">Launch Online Application</a></p>
-
-            <p>
-                <h4>Pay Dues</h4>
-                <div>Pay membership dues: $75<br/>
-                    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                        <input type="hidden" name="cmd" value="_s-xclick">
-                        <input type="hidden" name="hosted_button_id" value="PGH78GLM92Z8U">
-                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                    </form>
-                </div>
-            </p>
-
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h2>Pay Dues</h2>
+            <div>Pay membership dues: $75<br/>
+                <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="PGH78GLM92Z8U">
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                </form>
+            </div>
         </div>
     </div>
 </div><div class="clearfix"></div>
